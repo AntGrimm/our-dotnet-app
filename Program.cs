acf45a990358caf599cadbc06e0fe3cf253db527
Anthony Grimm <anthony.l.grimm@gmail.com>
2020-07-28 08:04:23 -0400

    what is your name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace our_dotnet_app
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      Console.WriteLine("Welcome to my program!");
+      Console.Write("\n");
+
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+
+      Console.WriteLine($"It is a pleasure to meet you, {name}.");
     }
   }
 }
